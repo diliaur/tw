be7c015757fa5d11a7103e0fa15dee10fbace661
diliaur <diliaur@hawaii.edu>
2015-04-13 01:07:41 -1000

    initial commit for 624 twitter project

---
+++ twitter gem code.rb
@@ -0,0 +1,8 @@
+# get hashtags off a Tweet object
+# returning tweets using #hashtags method on a tweet obj will return a
+#    Hashtag(s) object hash with tags inside of it.
+
+hashtags_obj.each do |h|
+	puts h.text # prints
+end
+
