1d1faf4497feb5f9762b47c960de71b72c96262d
diliaur <diliaur@hawaii.edu>
2015-04-15 12:24:54 -1000

    still experimenting

--- twitter gem code.rb
+++ twitter gem code.rb
@@ -2,7 +2,15 @@
 # returning tweets using #hashtags method on a tweet obj will return a
 #    Hashtag(s) object hash with tags inside of it.
 
-hashtags_obj.each do |h|
-	puts h.text # prints
+# can take single tweet object or collection of tweet objects?
+# um need to rethink this probably
+def get_tags (tweet)
+	tweet.each do |h|
+		puts h.text # prints, need to save to array or obj
+	end
 end
 
+# get tweets by tags
+
+#def get_tweets_by_tag ()
+#end
