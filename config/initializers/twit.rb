17b1953e77c24b81dc2d271d440c8587044ddf37
Diliaur. ¯\_(ツ)_/¯ <diliaur@users.noreply.github.com>
2015-04-18 02:25:50 -1000

    Delete twit.rb

--- config/initializers/twit.rb
+++
@@ -1,6 +0,0 @@
-TCLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key = "rk8mbamljRaiKlrK6uPRHcCp3"
-  config.consumer_secret = "weD0Makmy5eyTAjR1uhLq9rOqD0NBZK4QtiCsdkNTRzDfeKKNt"
-  config.access_token = "86730252-yKQhPQeruowSTXYJwEaCLdnZPFAUnx43KSWBCNNNK"
-  config.access_token_secret = "3RWKBeOXjkiILjas2ovU0o9OgQdnwKOtOt4qPsxMBcj42"
-end
