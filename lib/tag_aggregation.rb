6065c4d7b66a24b5d5d119fc463f99308860adad
diliaur <diliaur@hawaii.edu>
2015-04-25 15:59:02 -1000

    tag aggregation thoughts

--- lib/tag_aggregation.rb
+++ lib/tag_aggregation.rb
@@ -1,7 +1,19 @@
 module TagAggregation
 
 	# count all tags and store unique 
+	# if not unique update count(s) as well as other info
+	# start from ground up? or if have Tag passed in, can simply add count... 
+	#   but don't want to overcount. if anytime tag is passed in, it will update...
+	#   so need to periodically empty all and recount? seems ridiculous...
+	# use hash map?
+	# need some way to check if that PARTICULAR TAG has already been counted.
 	def self.update_tag_counts
-		puts Tag.all.first.content #ok so I CAN use models in other dirs
+		#puts Tag.all.first.content #ok so I CAN use models in other dirs
+		Tag.all.each do |t|
+			new_utag = Utag.create(content:t.content) #doesnt create if exists
+			if !new_utag.nil?
+				# update 
+			end
+		end
 	end
 end
