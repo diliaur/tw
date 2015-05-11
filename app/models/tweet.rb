719ce6bb657726bd0a0ed36912fde05c339edf14
diliaur <diliaur@hawaii.edu>
2015-05-11 11:59:36 -1000

    add newline cleaning for tweets in vna parse

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -46,25 +46,32 @@ class Tweet < ActiveRecord::Base
 	def self.user_mentions_to_vna
 		# open a file to write to?
 
-		file = File.open("ferguson_discussion.vna","w") do |f|
+		file = File.open("ferguson_discussion_replies.vna","w") do |f|
 			# write first line
 			f.write("*Node data\n")
 			f.write("ID tags tweet_content\n")
 			# grab all tweets that have user_mentions
-			tweets = Tweet.where("user_mentions is not null")
+			# tweets = Tweet.where("user_mentions is not null")
+			
 			#tweets = Tweet.where("user_mentions is not ? AND is_retweet = ?",nil,false) # >500
 			# ^ grab all user_mentions WITHOUT retweets
-			#tweets = Tweet.where("user_mentions is not ? AND is_reply = ?",nil,true) #>200
+			
+			tweets = Tweet.where("user_mentions is not ? AND is_reply = ? AND is_retweet = ?",nil,true,false) #>200
 			# ^ grab all user_mentions that are replies
+			
 			#tweets = Tweet.where("user_mentions is not ? AND is_reply is ? AND is_retweet = ?",
 			#	nil,nil,false)
 			#^ grab user_mentions were neither retweet nor reply
+			
+			#tweets = Tweet.where("user_mentions is not ? AND is_reply is ? and is_retweet = ?",nil,nil,true)
+			# ^ grab user_mentions where is retweet only
+			
 			# for each tweet
 			edges = ""
 			tweets.all.each do |t|
 				# user_screen_name is the originating node
 				# add hash tags (string?) as attribute onto origin node
-				f.write "#{t.user_screen_name} #{t.tags} \"#{t.content}\"\n"
+				f.write "#{t.user_screen_name} #{t.tags} \"#{t.content.gsub("\n","")}\"\n"
 				# grab user_mentions field
 				# for each user in user_mentions
 				t.user_mentions.split(",").each do |um|
